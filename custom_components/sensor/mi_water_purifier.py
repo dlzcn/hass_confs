232156ba24867c6ce43d4a5ab8c0b66a5b796d51
Haifeng CHEN <optical.dlz@gmail.com>
2018-12-04 20:08:03 +0800

    Try to fixed multithread bug in mi_water_purifier

--- custom_components/sensor/mi_water_purifier.py
+++ custom_components/sensor/mi_water_purifier.py
@@ -1,14 +1,18 @@
 """Support for Xiaomi water purifier."""
 import math
 import logging
+from datetime import timedelta, datetime
+from threading import Lock
 
-from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_TOKEN, )
+from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_TOKEN, CONF_SCAN_INTERVAL, )
 from homeassistant.helpers.entity import Entity
 from homeassistant.exceptions import PlatformNotReady
 
 _LOGGER = logging.getLogger(__name__)
 
 REQUIREMENTS = ['python-miio>=0.3.1']
+
+SCAN_INTERVAL = timedelta(seconds=1200)
 
 TAP_WATER_QUALITY = {'name': 'Tap water', 'key': 'ttds'}
 FILTERED_WATER_QUALITY = {'name': 'Filtered water', 'key': 'ftds'}
@@ -17,10 +21,12 @@ FRONT_ACTIVE_CARBON_FILTER_REMAINING = {'name': 'Front active carbon filter', 'k
 RO_FILTER_REMAINING = {'name': 'RO filter', 'key': 'rfd', 'days_key': 'rfp'}
 REAR_ACTIVE_CARBON_FILTER_REMAINING = {'name': 'Rear active carbon filter', 'key': 'rcfd', 'days_key': 'rcfp'}
 
-
-def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Perform the setup for Xiaomi water purifier."""
-    from miio import Device, DeviceException
+async def async_setup_platform(hass, config, async_add_entities,
+                               discovery_info=None):
+     """Perform the setup for Xiaomi water purifier."""
+    from miio import Device, DeviceException    
+    
+    cache = config.get(CONF_SCAN_INTERVAL, SCAN_INTERVAL).total_seconds()
 
     host = config.get(CONF_HOST)
     name = config.get(CONF_NAME)
@@ -31,8 +37,8 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     devices = []
     try:
         device = Device(host, token)
-        waterPurifier = XiaomiWaterPurifier(device, name)
-        devices.append(waterPurifier)
+        waterPurifier = XiaomiWaterPurifier(device, cache)
+
         devices.append(XiaomiWaterPurifierSensor(waterPurifier, TAP_WATER_QUALITY))
         devices.append(XiaomiWaterPurifierSensor(waterPurifier, FILTERED_WATER_QUALITY))
         devices.append(XiaomiWaterPurifierSensor(waterPurifier, PP_COTTON_FILTER_REMAINING))
@@ -43,7 +49,8 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         _LOGGER.exception('Fail to setup Xiaomi water purifier')
         raise PlatformNotReady
 
-    add_devices(devices)
+    async_add_entities(devivces)
+
 
 class XiaomiWaterPurifierSensor(Entity):
     """Representation of a XiaomiWaterPurifierSensor."""
@@ -103,8 +110,8 @@ class XiaomiWaterPurifierSensor(Entity):
         return self._available
 
     def parse_data(self):
-        if self._waterPurifier._data:
-            self._data = self._waterPurifier._data
+        self._data = self._waterPurifier.read()
+        if self._data:
             self._state = self._data[self._data_key['key']]
             self._available = True
         else:
@@ -114,90 +121,50 @@ class XiaomiWaterPurifierSensor(Entity):
         """Get the latest data and updates the states."""
         self.parse_data()
 
-class XiaomiWaterPurifier(Entity):
+class XiaomiWaterPurifier():
     """Representation of a XiaomiWaterPurifier."""
 
-    def __init__(self, device, name):
+    def __init__(self, device, cache_timeout):
         """Initialize the XiaomiWaterPurifier."""
-        self._state = None
         self._device = device
-        self._name = name
-        self._available = False
+        self._cache = {}
+        self._cache_timeout = timedelta(seconds=cache_timeout)
+        self._last_read = None
+        self.lock = Lock()
         self.parse_data()
-
-    @property
-    def name(self):
-        """Return the name of the device."""
-        return self._name
-
-    @property
-    def icon(self):
-        """Icon to use in the frontend, if any."""
-        return 'mdi:water'
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return 'TDS'
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        return self._state
-
-    @property
-    def hidden(self) -> bool:
-        """Return True if the entity should be hidden from UIs."""
-        return True
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes of the last update."""
-        attrs = {}
-        attrs[TAP_WATER_QUALITY['name']] = '{}TDS'.format(self._data[TAP_WATER_QUALITY['key']])
-        attrs[PP_COTTON_FILTER_REMAINING['name']] = '{}%'.format(self._data[PP_COTTON_FILTER_REMAINING['key']])
-        attrs[FRONT_ACTIVE_CARBON_FILTER_REMAINING['name']] = '{}%'.format(self._data[FRONT_ACTIVE_CARBON_FILTER_REMAINING['key']])
-        attrs[RO_FILTER_REMAINING['name']] = '{}%'.format(self._data[RO_FILTER_REMAINING['key']])
-        attrs[REAR_ACTIVE_CARBON_FILTER_REMAINING['name']] = '{}%'.format(self._data[REAR_ACTIVE_CARBON_FILTER_REMAINING['key']])
-
-        return attrs
-
-    @property
-    def available(self):
-        """Return true when state is known."""
-        return self._available
 
     def parse_data(self):
         """Parse data."""
         from miio import DeviceException
-        try:
-            data = {}
-            status = self._device.send('get_prop', [])
-            data[TAP_WATER_QUALITY['key']] = status[0]
-            data[FILTERED_WATER_QUALITY['key']] = status[1]
-            pfd = int((status[11] - status[3]) / 24)
-            data[PP_COTTON_FILTER_REMAINING['days_key']] = pfd
-            data[PP_COTTON_FILTER_REMAINING['key']] = math.floor(pfd * 24 * 100 / status[11])
-            fcfd = int((status[13] - status[5]) / 24)
-            data[FRONT_ACTIVE_CARBON_FILTER_REMAINING['days_key']] = fcfd
-            data[FRONT_ACTIVE_CARBON_FILTER_REMAINING['key']] = math.floor(fcfd * 24 * 100 / status[13])
-            rfd = int((status[15] - status[7]) / 24)
-            data[RO_FILTER_REMAINING['days_key']] = rfd
-            data[RO_FILTER_REMAINING['key']] = math.floor(rfd * 24 * 100 / status[15])
-            rcfd = int((status[17] - status[9]) / 24)
-            data[REAR_ACTIVE_CARBON_FILTER_REMAINING['days_key']] = rcfd
-            data[REAR_ACTIVE_CARBON_FILTER_REMAINING['key']] = math.floor(rcfd * 24 * 100 / status[17])
+        if not self._cache or force_update or \
+            (datetime.now() - self._cache_timeout > self._last_read):
+            self._last_read = datetime.now()
+            try:
+                data = {}
+                status = self._device.send('get_prop', [])
+                data[TAP_WATER_QUALITY['key']] = status[0]
+                data[FILTERED_WATER_QUALITY['key']] = status[1]
+                pfd = int((status[11] - status[3]) / 24)
+                data[PP_COTTON_FILTER_REMAINING['days_key']] = pfd
+                data[PP_COTTON_FILTER_REMAINING['key']] = math.floor(pfd * 24 * 100 / status[11])
+                fcfd = int((status[13] - status[5]) / 24)
+                data[FRONT_ACTIVE_CARBON_FILTER_REMAINING['days_key']] = fcfd
+                data[FRONT_ACTIVE_CARBON_FILTER_REMAINING['key']] = math.floor(fcfd * 24 * 100 / status[13])
+                rfd = int((status[15] - status[7]) / 24)
+                data[RO_FILTER_REMAINING['days_key']] = rfd
+                data[RO_FILTER_REMAINING['key']] = math.floor(rfd * 24 * 100 / status[15])
+                rcfd = int((status[17] - status[9]) / 24)
+                data[REAR_ACTIVE_CARBON_FILTER_REMAINING['days_key']] = rcfd
+                data[REAR_ACTIVE_CARBON_FILTER_REMAINING['key']] = math.floor(rcfd * 24 * 100 / status[17])
 
-            self._data = data
-            self._state = self._data[FILTERED_WATER_QUALITY['key']]
-            self._available = True
-        except DeviceException:
-            _LOGGER.exception('Fail to get_prop from Xiaomi water purifier')
-            self._data = None
-            self._state = None
-            self._available = False
+                self._cache = data
+            except DeviceException:
+                _LOGGER.exception('Fail to get_prop from Xiaomi water purifier')
+                self._cache = None
             #raise PlatformNotReady
 
-    def update(self):
+    def read(self):
         """Get the latest data and updates the states."""
-        self.parse_data()
+        with self.lock:
+            self.parse_data()
+            return self._cache
